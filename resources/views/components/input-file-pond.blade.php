920fe4842726d3dda52c806136afd0240713245b
arthaputra <manager.arthachan@gmail.com>
2021-07-30 20:32:02 +0800

    uploads

--- resources/views/components/input-file-pond.blade.php
+++ resources/views/components/input-file-pond.blade.php
@@ -12,16 +12,22 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://unpkg.com/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js"></script>
         <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
+        <script src="https://unpkg.com/filepond-plugin-image-resize/dist/filepond-plugin-image-resize.js"></script>
         <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
         <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
         <script>
             FilePond.registerPlugin(FilePondPluginFileValidateType);
             FilePond.registerPlugin(FilePondPluginFileValidateSize);
             FilePond.registerPlugin(FilePondPluginImagePreview);
+            FilePond.registerPlugin(FilePondPluginImageResize);
         </script>
         <script>
            const post = FilePond.create(document.querySelector('input[type=file]'));
            post.setOptions({
+            allowImageResize: true,
+            imageResizeTargetWidth: 1280,
+            imageResizeTargetHeight: 720,
+            imageResizeMode: 'cover',
             server: {
                 url: "{{ route('upload.images') }}",
                 headers: {
