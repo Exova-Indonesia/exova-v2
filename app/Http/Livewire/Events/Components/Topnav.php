a2ae537cd189fef9c18277d29db2e8f40a3fda9a
arthaputra <manager.arthachan@gmail.com>
2021-08-14 21:18:31 +0800

    burger button

--- app/Http/Livewire/Events/Components/Topnav.php
+++ app/Http/Livewire/Events/Components/Topnav.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class Topnav extends Component
 {
+    public $mobileMenu = false;
+
     public function render()
     {
         return view('livewire.events.components.topnav');
--- resources/views/livewire/events/components/topnav.blade.php
+++ resources/views/livewire/events/components/topnav.blade.php
@@ -79,30 +79,25 @@
     </ul>
     <!-- Mobile menu -->
     <div class="lg:hidden">
-      <button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50">
+      <button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50" wire:click="$toggle('mobileMenu')">
         <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
           <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
           <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
           <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
         </svg>
       </button>
-      <!-- Mobile menu dropdown 
-      <div class="absolute top-0 left-0 w-full">
+      @if($mobileMenu)
+      <!-- Mobile menu dropdown -->
+      <div class="absolute top-0 left-0 w-full z-20">
         <div class="p-5 bg-white border rounded shadow-sm">
           <div class="flex items-center justify-between mb-4">
             <div>
               <a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
-                <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
-                  <rect x="3" y="1" width="7" height="12"></rect>
-                  <rect x="3" y="17" width="7" height="6"></rect>
-                  <rect x="14" y="1" width="7" height="6"></rect>
-                  <rect x="14" y="11" width="7" height="12"></rect>
-                </svg>
-                <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
+                <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Exova Event</span>
               </a>
             </div>
             <div>
-              <button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+              <button wire:click="$toggle('mobileMenu')" aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
                 <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
                   <path
                     fill="currentColor"
@@ -114,25 +109,15 @@
           </div>
           <nav>
             <ul class="space-y-4">
-              <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
-              <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
-              <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
-              <li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
-              <li>
-                <a
-                  href="/"
-                  class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                  aria-label="Sign up"
-                  title="Sign up"
-                >
-                  Sign up
-                </a>
-              </li>
+              <li><a href="{{ url('event') }}" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Home</a></li>
+              <li><a href="{{ url('event/webinar') }}" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Webinar</a></li>
+              <li><a href="{{ url('event/competition') }}" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Kompetisi</a></li>
+              <li><a href="{{ url('event/training') }}" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pelatihan</a></li>
             </ul>
           </nav>
         </div>
       </div>
-      -->
+      @endif
     </div>
   </div>
 </div>
