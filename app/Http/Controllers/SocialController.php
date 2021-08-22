ca4587aabc13e53aa6e3573e137abd480d05238d
arthaputra <manager.arthachan@gmail.com>
2021-08-22 15:54:54 +0800

    bug notif email

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -54,7 +54,6 @@ class SocialController extends Controller
                 'provider_id' => $user->id,
                 'role_id' => User::IS_CUST,
             ]);
-            event(new Registered($data));
             return $data;
         }
     }
