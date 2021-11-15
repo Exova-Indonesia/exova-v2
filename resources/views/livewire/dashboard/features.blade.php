affcc09a2715c892862050ec09dbe2cb0f80e2b0
arthaputra <manager.arthachan@gmail.com>
2021-11-15 21:21:02 +0800

    apene

--- resources/views/livewire/dashboard/features.blade.php
+++ resources/views/livewire/dashboard/features.blade.php
@@ -1,7 +1,7 @@
 <div>
 <!-- Section 3 -->
 <section class="bg-gray-50 pb-32 pt-12">
-   <div class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16">
+   <div class="container items-center max-w-6xl px-4 mx-auto sm:px-20 md:px-32 lg:px-16">
       <div class="flex flex-wrap items-center -mx-3">
          <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
             <div class="w-full lg:max-w-md">
@@ -12,13 +12,13 @@
                      <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                      </svg>
-                     <span class="font-medium text-gray-500">Lebih mudah mencari jasa dokumentasi di bidang traditional & culture</span>
+                     <span class="font-medium text-gray-500">Lebih mudah mencari videographer & photographer</span>
                   </li>
                   <li class="flex items-center py-2 space-x-4 xl:py-3">
                      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                      </svg>
-                     <span class="font-medium text-gray-500">Seller bebas menjual jasa atas nama brand masing - masing</span>
+                     <span class="font-medium text-gray-500">Freelancer bebas menjual jasa atas nama brand masing - masing</span>
                   </li>
                   <li class="flex items-center py-2 space-x-4 xl:py-3">
                      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -30,7 +30,7 @@
                      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
                      </svg>
-                     <span class="font-medium text-gray-500">Bebas membandingkan harga & kualitas</span>
+                     <span class="font-medium text-gray-500">Bebas memilih photo/videographer, membandingkan harga & kualitas</span>
                   </li>
                   <li class="flex items-center py-2 space-x-4 xl:py-3">
                      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
